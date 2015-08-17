2ab1b7c274fdb81858ef6ba2bd52f015a3c158dc
joaorubenssoares <joaorubens.soares@prodap.com.br>
2015-08-17 12:07:47 -0300

    {"T": "D", "M": [], "BD":[TAURUS-CRIA-MOBILE],”O”:” MIGRANDO PARA  ANDROID STUDIO"}

--- TaurusMobile/app/src/main/java/br/com/prodap/taurusmobile/Annotation/AColumn.java
+++
@@ -1,7 +0,0 @@
-package br.com.prodap.taurusmobile.Annotation;
-
-public @interface AColumn {
-	
-	public int position();
-
-}
--- TaurusMobile/src/br/com/prodap/taurusmobile/util/Constantes.java
+++
@@ -1,7 +0,0 @@
-package br.com.prodap.taurusmobile.util;
-
-public class Constantes {				
-	public static final String SERVER = "http://192.168.0.150/TaurusWebService/TaurusService.svc/";
-	public static final String GET = SERVER + "listaAnimaisJson";
-	public static final String POST = SERVER + "SendJson";
-}
